a8e4cf977ddb3fa343d552b901b88e949642ded5
RifatDIU <monirbcd3918@gmail.com>
2026-04-19 13:09:50 +0600

    ADD River

--- main.cpp
+++ main.cpp
@@ -5,7 +5,6 @@
 
 void init()
 {
-    // Background color (set to white or sky blue)
     glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
     glOrtho(-210, 210, -220, 310, -210, 310);
 }
@@ -14,10 +13,9 @@ void display()
 {
     glClear(GL_COLOR_BUFFER_BIT);
 
-    //-----------------------------------FIELD ONLY------------------------------
+    //-----------------------------------FIELD (MATH)------------------------------
     glBegin(GL_POLYGON);
-        glColor3ub(0, 100, 0); // Green grass color
-
+        glColor3ub(0, 100, 0); // Green
         glVertex2i(-200, 100);
         glVertex2i(-100, 160);
         glVertex2i(0, 100);
@@ -25,14 +23,33 @@ void display()
         glVertex2i(100, 180);
         glVertex2i(200, 100);
 
-        glColor3ub(255, 215, 0); // Fading into a Gold color at the bottom
+        glColor3ub(255, 215, 0); // Gold
         glVertex2i(200, -200);
         glVertex2i(-200, -200);
-
-        glColor3ub(255, 215, 0); // Closing the polygon
         glVertex2i(-200, 100);
     glEnd();
-    //---------------------------------------------------------------------------
+
+    //------------------------------------------RIVER--------------------------------------------------
+    glBegin(GL_POLYGON);
+        glColor3ub(30, 144, 255); // Deep Sky Blue
+        glVertex2i(-200, -50);
+        glVertex2i(200, -30);
+        
+        glColor3ub(0, 0, 128);    // Navy Blue (Niche dark effect)
+        glVertex2i(200, -200);
+        glVertex2i(-200, -200);
+        glVertex2i(-200, -50);
+    glEnd();
+
+    //---------------------------------------RIVER BORDER---------------------------------------------
+    glBegin(GL_POLYGON);
+        glColor3ub(128, 128, 0); // Olive color border
+        glVertex2i(-200, -45);
+        glVertex2i(200, -25);
+        glVertex2i(200, -30);
+        glVertex2i(-200, -50);
+        glVertex2i(-200, -45);
+    glEnd();
 
     glFlush();
 }
@@ -43,7 +60,7 @@ int main(int argc, char *argv[])
     glutInitWindowSize(1200, 800);
     glutInitWindowPosition(10, 10);
     glutInitDisplayMode(GLUT_RGB | GLUT_SINGLE);
-    glutCreateWindow("Field Only Output");
+    glutCreateWindow("Field and River Only");
     init();
     glutDisplayFunc(display);
     glutMainLoop();
