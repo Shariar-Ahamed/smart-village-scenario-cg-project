f6c49c68b89c08e3d5353e0c3f0de683cbd99666
sultana1682 <asma2305101682@diu.edu.bd>
2026-04-20 18:49:19 +0600

    tree

--- main.cpp
+++ main.cpp
@@ -111,6 +111,70 @@ void display()
     tx+=.05;
     if(tx>200) tx=-200;
 
+//---------------- TREE ----------------
+    // small tree
+    glColor3ub(139,69,19);
+    glRecti(-20,200,-13,140);
+
+    glColor3ub(0,100,0);
+    sun(-30,190);
+    sun(0,190);
+    sun(-10,210);
+    sun(-30,175);
+    sun(0,170);
+
+    // big tree trunk
+    glBegin(GL_POLYGON);
+        glColor3ub(139,69,19);
+        glVertex2i(-170,160);
+        glVertex2i(-168,120);
+        glVertex2i(-178,40);
+        glVertex2i(-145,40);
+        glVertex2i(-153,120);
+        glVertex2i(-150,160);
+    glEnd();
+
+    // branches
+    glBegin(GL_POLYGON);
+        glVertex2i(-153,100);
+        glVertex2i(-100,200);
+        glVertex2i(-95,200);
+        glVertex2i(-153,80);
+    glEnd();
+
+    glBegin(GL_POLYGON);
+        glVertex2i(-170,160);
+        glVertex2i(-185,210);
+        glVertex2i(-190,210);
+        glVertex2i(-168,90);
+    glEnd();
+
+    glBegin(GL_POLYGON);
+        glVertex2i(-160,160);
+        glVertex2i(-150,210);
+        glVertex2i(-140,210);
+        glVertex2i(-150,160);
+    glEnd();
+
+    // leaves
+    glColor3ub(0,128,0);
+
+    sun(-95,200);
+    sun(-80,180);
+    sun(-110,180);
+    sun(-120,200);
+
+    sun(-150,200);
+    sun(-130,180);
+    sun(-125,220);
+    sun(-140,230);
+
+    sun(-190,210);
+    sun(-180,200);
+    sun(-175,225);
+    sun(-195,190);
+
+//---------------- END ----------------
     glutPostRedisplay();
     glFlush();
 }
@@ -130,3 +194,4 @@ int main(int argc,char *argv[])
 
     return 0;
 }
+
